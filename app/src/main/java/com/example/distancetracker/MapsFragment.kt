e7c26273db077233d80a3858a3ea6e7d2926ae98
Nobel Elan <nobelelan@gmail.com>
2023-02-12 15:25:50 +0600

    implemented my location layer

--- app/src/main/java/com/example/distancetracker/MapsFragment.kt
+++ app/src/main/java/com/example/distancetracker/MapsFragment.kt
@@ -1,24 +1,33 @@
 package com.example.distancetracker
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.example.distancetracker.databinding.FragmentMapsBinding
+import com.example.distancetracker.utils.ExtensionFunctions.hide
+import com.example.distancetracker.utils.ExtensionFunctions.show
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment(), OnMapReadyCallback, OnMyLocationButtonClickListener {
 
     private var _binding: FragmentMapsBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var map: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -41,8 +50,29 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         mapFragment?.getMapAsync(this)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        map.isMyLocationEnabled = true
+        map.setOnMyLocationButtonClickListener(this)
+        map.uiSettings.apply {
+            isZoomControlsEnabled = false
+            isZoomGesturesEnabled = false
+            isRotateGesturesEnabled = false
+            isTiltGesturesEnabled = false
+            isCompassEnabled = false
+            isScrollGesturesEnabled = false
+        }
+    }
 
+    override fun onMyLocationButtonClick(): Boolean {
+        binding.txtHint.animate().alpha(0f).duration = 1500
+        lifecycleScope.launch {
+            delay(2500)
+            binding.txtHint.hide()
+            binding.btnStart.show()
+        }
+        return false
     }
 
     override fun onDestroyView() {
