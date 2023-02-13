307461f6d8cb5572fa7e8f99a165e9006b1eeb1e
Nobel Elan <nobelelan@gmail.com>
2023-02-14 00:07:46 +0600

    result fragment designed

--- app/src/main/java/com/example/distancetracker/ui/result/ResultFragment.kt
+++ app/src/main/java/com/example/distancetracker/ui/result/ResultFragment.kt
@@ -6,8 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.distancetracker.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class ResultFragment : Fragment() {
+class ResultFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
