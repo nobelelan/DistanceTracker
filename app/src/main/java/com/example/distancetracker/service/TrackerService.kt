d598d9968cc5491a3906203a34c1ca2332c5a521
Nobel Elan <nobelelan@gmail.com>
2023-02-13 12:51:30 +0600

    start location updates

--- app/src/main/java/com/example/distancetracker/service/TrackerService.kt
+++ app/src/main/java/com/example/distancetracker/service/TrackerService.kt
@@ -1,18 +1,30 @@
 package com.example.distancetracker.service
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.content.Intent
 import android.os.Build
+import android.os.Looper
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
 import com.example.distancetracker.utils.Constants.ACTION_SERVICE_START
 import com.example.distancetracker.utils.Constants.ACTION_SERVICE_STOP
+import com.example.distancetracker.utils.Constants.LOCATION_FASTEST_UPDATE_INTERVAL
+import com.example.distancetracker.utils.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.distancetracker.utils.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.distancetracker.utils.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.distancetracker.utils.Constants.NOTIFICATION_ID
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -24,8 +36,23 @@ class TrackerService: LifecycleService() {
 
     @Inject
     lateinit var notificationManager: NotificationManager
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     companion object{
         val started = MutableLiveData<Boolean>()
+    }
+
+    private val locationCallback = object : LocationCallback(){
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            result.locations.let { locations ->
+                for (location in locations){
+                    val newLatLng = LatLng(location.latitude, location.longitude)
+                    Log.d("TrackerService", newLatLng.toString())
+                }
+            }
+        }
     }
 
     private fun setInitialValues(){
@@ -34,6 +61,7 @@ class TrackerService: LifecycleService() {
 
     override fun onCreate() {
         setInitialValues()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
         super.onCreate()
     }
 
@@ -43,6 +71,7 @@ class TrackerService: LifecycleService() {
                 ACTION_SERVICE_START -> {
                     started.postValue(true)
                     startForegroundService()
+                    startLocationUpdates()
                 }
                 ACTION_SERVICE_STOP -> {
                     started.postValue(false)
@@ -56,6 +85,20 @@ class TrackerService: LifecycleService() {
     private fun startForegroundService(){
         createNotificationChannel()
         startForeground(NOTIFICATION_ID, notification.build())
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates(){
+        val locationRequest = LocationRequest().apply {
+            interval = LOCATION_UPDATE_INTERVAL
+            fastestInterval = LOCATION_FASTEST_UPDATE_INTERVAL
+            priority = PRIORITY_HIGH_ACCURACY
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
     }
 
     private fun createNotificationChannel(){
